cf06d31d212f6949ddbfeb469fa39272c27be428
Oxters <oxters168@gmail.com>
2021-10-24 19:59:38 -0400

    Created a noise tile by extending from base tile. Made it possible to switch between debugging and actual terrain to help with procedural generation.

--- Assets/Core/Scripts/TerrainGenerator.cs
+++ Assets/Core/Scripts/TerrainGenerator.cs
@@ -2,25 +2,42 @@ using UnityEngine;
 using UnityHelpers;
 using UnityEngine.Tilemaps;
 using System.Linq;
-using System.Collections;
 
 public class TerrainGenerator : MonoBehaviour
 {
     private CompositeCollider2D _physicsBounds;
     private CompositeCollider2D PhysicsBounds { get { if (_physicsBounds == null) _physicsBounds = GetComponentInChildren<CompositeCollider2D>(); return _physicsBounds; } }
 
-    public float noiseMin = 0.42f;
+    public float noiseThreshold = 0.42f;
+    public bool invertNoise;
+    private bool prevInvertNoise;
+    [Space(10)]
+    public Tile noiseTile;
+    public FastNoise.NoiseType noiseType = FastNoise.NoiseType.Perlin;
     public float noiseFrequency = 0.01f;
-    public Tilemap foreground, physical, background;
+    private float prevNoiseFrequency = 0.01f;
+    public int noiseOctaves = 3;
+    public float noisePower = 1;
+    private float prevNoisePower = 1;
+    public bool debugNoise;
+    private bool prevDebugNoise;
+
+    [Space(10)]
+    public Tilemap foreground;
+    public Tilemap physical;
+    public Tilemap background;
 
     [Space(10)]
     public Transform target;
-    // [Tooltip("The number of horizontal tiles per chunk. Should be even, if not even will be floor(half) x 2 of the odd number to make it even")]
-    // public int chunkWidth = 8;
-    // [Tooltip("The number of vertical tiles per chunk. Should be even, if not even will be floor(half) x 2 of the odd number to make it even")]
-    // public int chunkHeight = 8;
-    // [Tooltip("How many chunks beyond the current to render (0 renders 1 chunk, 1 renders 9 chunks, 2 renders 25 chunks...)")]
-    // public int chunkRenderDistance = 1;
+    [Tooltip("The number of horizontal tiles per chunk. Should be even, if not even will be floor(half) x 2 of the odd number to make it even")]
+    public int chunkWidth = 8;
+    private int prevChunkWidth = 8;
+    [Tooltip("The number of vertical tiles per chunk. Should be even, if not even will be floor(half) x 2 of the odd number to make it even")]
+    public int chunkHeight = 8;
+    private int prevChunkHeight = 8;
+    [Tooltip("How many chunks beyond the current to render (0 renders 1 chunk, 1 renders 9 chunks, 2 renders 25 chunks...)")]
+    public int chunkRenderDistance = 1;
+    private int prevChunkRenderDistance = 1;
 
     public BiomeInfo currentBiome;
     private BackgroundLoop currentBackground;
@@ -40,14 +57,16 @@ public class TerrainGenerator : MonoBehaviour
     void Start()
     {
         noise = new FastNoise(WorldData.seed);
-        noise.SetNoiseType(FastNoise.NoiseType.Perlin);
+        noise.SetNoiseType(noiseType);
     }
     void Update()
     {
-        WorldData.chunkWidth = (WorldData.chunkWidth / 2) * 2; //Turns the width into an even number (since integer division is floored by default)
-        WorldData.chunkHeight = (WorldData.chunkHeight / 2) * 2; //Turns the height into an even number (since integer division is floored by default)
+        chunkWidth = (chunkWidth / 2) * 2; //Turns the width into an even number (since integer division is floored by default)
+        chunkHeight = (chunkHeight / 2) * 2; //Turns the height into an even number (since integer division is floored by default)
         prevChunk = chunk;
-        chunk = WorldData.GetChunkFromPosition(target.position);
+        chunk = new Vector2Int(Mathf.FloorToInt(((target.position.x - (chunkWidth / 2f)) / chunkWidth) + 1),
+            Mathf.FloorToInt(((target.position.y - (chunkHeight / 2f)) / chunkHeight) + 1));
+        // chunk = GetChunkFromPosition(target.position);
         DebugPanel.Log("Position", target.position.xy(), 5);
         DebugPanel.Log("Chunk", chunk, 5);
 
@@ -70,38 +89,56 @@ public class TerrainGenerator : MonoBehaviour
         var foregroundTiles = new Tile[tilesToBeDrawn.Length];
         var physicalTiles = new Tile[tilesToBeDrawn.Length];
         var backgroundTiles = new Tile[tilesToBeDrawn.Length];
+        noise.SetNoiseType(noiseType);
+        noise.SetFractalOctaves(noiseOctaves);
+        noise.SetFrequency(noiseFrequency);
 
         for (int i = 0; i < physicalTiles.Length; i++)
         {
             var currentTilePos = tilesToBeDrawn[i];
-            noise.SetFrequency(noiseFrequency);
-            float perlin = (noise.GetValue(currentTilePos.x * 4001, currentTilePos.y * 7121) + 1) / 2;
+            float rawNoise = noise.GetNoise(currentTilePos.x, currentTilePos.y);
+            // float someting = ((noise.GetNoise(currentTilePos.x + 2, currentTilePos.y) + 1) / 2);
+            // float sometingElse = ((noise.GetNoise(currentTilePos.x, currentTilePos.y + 1) + 1) / 2);
+            float perlin = Mathf.Pow((rawNoise + 1) / 2, noisePower);
             // this.layer1Amplitude * noise.perlin2(col * this.layer1Frequency * this.layer1Multiplier, row * this.layer1Frequency * this.layer1Multiplier);
 
-            if (currentTilePos.y == 1)
+            if (!debugNoise)
             {
-                if (!WorldData.IsDug(currentTilePos + Vector3Int.down))
-                    foregroundTiles[i] = currentBiome.ornamentalTile;
-            }
-            
-            if (currentTilePos.y == 0)
-            {
-                if (!WorldData.IsDug(currentTilePos))
-                    physicalTiles[i] = currentBiome.surfaceTile;
-                backgroundTiles[i] = currentBiome.backgroundTile;
-            }
+                if (currentTilePos.y == 1)
+                {
+                    if (!WorldData.IsDug(currentTilePos + Vector3Int.down))
+                        foregroundTiles[i] = currentBiome.ornamentalTile;
+                }
+                
+                if (currentTilePos.y == 0)
+                {
+                    if (!WorldData.IsDug(currentTilePos))
+                        physicalTiles[i] = currentBiome.surfaceTile;
+                    backgroundTiles[i] = currentBiome.backgroundTile;
+                }
 
-            if (currentTilePos.y < 0)
+                if (currentTilePos.y < 0)
+                {
+                    if ((invertNoise && perlin <= noiseThreshold || !invertNoise && perlin >= noiseThreshold) && !WorldData.IsDug(currentTilePos))
+                        physicalTiles[i] = currentBiome.undergroundTile;
+                    backgroundTiles[i] = currentBiome.backgroundTile;
+                }
+            }
+            else
             {
-                if (perlin >= noiseMin && !WorldData.IsDug(currentTilePos))
-                    physicalTiles[i] = currentBiome.undergroundTile;
-                backgroundTiles[i] = currentBiome.backgroundTile;
+                physical.SetTile(currentTilePos, noiseTile);
+                physical.SetTileFlags(currentTilePos, TileFlags.None);
+                physical.SetColor(currentTilePos, new Color(perlin, perlin, perlin, 1));
+                physical.RefreshTile(currentTilePos);
             }
         }
-
-        foreground.SetTiles(tilesToBeDrawn, foregroundTiles);
-        physical.SetTiles(tilesToBeDrawn, physicalTiles);
-        background.SetTiles(tilesToBeDrawn, backgroundTiles);
+        
+        if (!debugNoise)
+        {
+            foreground.SetTiles(tilesToBeDrawn, foregroundTiles);
+            physical.SetTiles(tilesToBeDrawn, physicalTiles);
+            background.SetTiles(tilesToBeDrawn, backgroundTiles);
+        }
     }
     private void ClearTiles(params Vector3Int[] tilesToBeCleared)
     {
@@ -113,16 +150,17 @@ public class TerrainGenerator : MonoBehaviour
 
     private void GenerateTerrain()
     {
-        if (firstDraw || chunk != prevChunk)
+        bool changeHappens = firstDraw || chunk != prevChunk || chunkWidth != prevChunkWidth || chunkHeight != prevChunkHeight || chunkRenderDistance != prevChunkRenderDistance || !Mathf.Approximately(noiseFrequency, prevNoiseFrequency) || !Mathf.Approximately(noisePower, prevNoisePower) || invertNoise != prevInvertNoise || debugNoise != prevDebugNoise;
+        if (changeHappens)
         {
             // Debug.Log(chunkX + ", " + chunkY);
 
-            var newTilePositions = GetChunkTilePositions(chunk.x, chunk.y, WorldData.chunkWidth, WorldData.chunkHeight, WorldData.chunkRenderDistance); //Get all new tile indices
+            var newTilePositions = GetChunkTilePositions(chunk.x, chunk.y, chunkWidth, chunkHeight, chunkRenderDistance); //Get all new tile indices
             Vector3Int[] tilesToBeDrawn;
 
-            if (!firstDraw)
+            if (!changeHappens)
             {
-                var oldTilePositions = GetChunkTilePositions(prevChunk.x, prevChunk.y, WorldData.chunkWidth, WorldData.chunkHeight, WorldData.chunkRenderDistance); //Get all old tile indices
+                var oldTilePositions = GetChunkTilePositions(prevChunk.x, prevChunk.y, chunkWidth, chunkHeight, chunkRenderDistance); //Get all old tile indices
                 tilesToBeDrawn = newTilePositions.Except(oldTilePositions).ToArray(); //Get exclusive new tiles
 
                 var tilesToBeCleared = oldTilePositions.Except(newTilePositions).ToArray(); //Get exclusive old tiles
@@ -137,16 +175,13 @@ public class TerrainGenerator : MonoBehaviour
             }
 
             DrawTiles(tilesToBeDrawn); //Add exclusive new tiles to grid
-            // StartCoroutine(UpdateColliderNextFrame());
-            // PhysicsBounds.GenerateGeometry(); //Regenerate collider
 
             // firstDraw = false;
+            prevNoiseFrequency = noiseFrequency;
+            prevNoisePower = noisePower;
+            prevInvertNoise = invertNoise;
+            prevDebugNoise = debugNoise;
         }
-    }
-    private IEnumerator UpdateColliderNextFrame()
-    {
-        yield return null;
-        PhysicsBounds.GenerateGeometry();
     }
 
     private static Vector3Int[] GetChunkTilePositions(int chunkX, int chunkY, int chunkWidth, int chunkHeight, int chunkRenderDistance)
--- Assets/Core/Scripts/WorldData.cs
+++ Assets/Core/Scripts/WorldData.cs
@@ -5,18 +5,18 @@ public class WorldData
 {
     public static int seed = 1337;
 
-    /// <summary>
-    /// The number of horizontal tiles per chunk. Should be even, if not even will be floor(half) x 2 of the odd number to make it even
-    /// </summary>
-    public static int chunkWidth = 2; //Is the global setting and will be used to set the terrain generator values
-    /// <summary>
-    /// The number of vertical tiles per chunk. Should be even, if not even will be floor(half) x 2 of the odd number to make it even
-    /// </summary>
-    public static int chunkHeight = 2; //Is the global setting and will be used to set the terrain generator values
-    /// <summary>
-    /// How many chunks beyond the current to render (0 renders 1 chunk, 1 renders 9 chunks, 2 renders 25 chunks...)
-    /// </summary>
-    public static int chunkRenderDistance = 8; //Is the global setting and will be used to set the terrain generator values
+    // /// <summary>
+    // /// The number of horizontal tiles per chunk. Should be even, if not even will be floor(half) x 2 of the odd number to make it even
+    // /// </summary>
+    // public static int chunkWidth = 2; //Is the global setting and will be used to set the terrain generator values
+    // /// <summary>
+    // /// The number of vertical tiles per chunk. Should be even, if not even will be floor(half) x 2 of the odd number to make it even
+    // /// </summary>
+    // public static int chunkHeight = 2; //Is the global setting and will be used to set the terrain generator values
+    // /// <summary>
+    // /// How many chunks beyond the current to render (0 renders 1 chunk, 1 renders 9 chunks, 2 renders 25 chunks...)
+    // /// </summary>
+    // public static int chunkRenderDistance = 8; //Is the global setting and will be used to set the terrain generator values
 
     // private static Dictionary<Vector2Int, List<Vector2Int>> dug = new Dictionary<Vector2Int, List<Vector2Int>>(); //The tiles that have been dug around the world
     private static List<Vector2Int> dug = new List<Vector2Int>();
@@ -49,24 +49,24 @@ public class WorldData
         return IsDug(new Vector2Int(tilePosition.x, tilePosition.y));
     }
 
-    public static Vector2Int GetChunkFromTile(Vector2Int tilePosition)
-    {
-        int chunkX = ((tilePosition.x - (chunkWidth / 2)) / chunkWidth) + 1;
-        int chunkY = ((tilePosition.y - (chunkHeight / 2)) / chunkHeight) + 1;
-        return new Vector2Int(chunkX, chunkY);
-    }
-    public static Vector2Int GetChunkFromTile(Vector3Int tilePosition)
-    {
-        return GetChunkFromTile(new Vector2Int(tilePosition.x, tilePosition.y));
-    }
-    public static Vector2Int GetChunkFromPosition(Vector2 position)
-    {
-        int chunkX = Mathf.FloorToInt(((position.x - (chunkWidth / 2f)) / chunkWidth) + 1);
-        int chunkY = Mathf.FloorToInt(((position.y - (chunkHeight / 2f)) / chunkHeight) + 1);
-        return new Vector2Int(chunkX, chunkY);
-    }
-    public static Vector2Int GetChunkFromPosition(Vector3 position)
-    {
-        return GetChunkFromPosition(new Vector2(position.x, position.y));
-    }
+    // public static Vector2Int GetChunkFromTile(Vector2Int tilePosition)
+    // {
+    //     int chunkX = ((tilePosition.x - (chunkWidth / 2)) / chunkWidth) + 1;
+    //     int chunkY = ((tilePosition.y - (chunkHeight / 2)) / chunkHeight) + 1;
+    //     return new Vector2Int(chunkX, chunkY);
+    // }
+    // public static Vector2Int GetChunkFromTile(Vector3Int tilePosition)
+    // {
+    //     return GetChunkFromTile(new Vector2Int(tilePosition.x, tilePosition.y));
+    // }
+    // public static Vector2Int GetChunkFromPosition(Vector2 position)
+    // {
+    //     int chunkX = Mathf.FloorToInt(((position.x - (chunkWidth / 2f)) / chunkWidth) + 1);
+    //     int chunkY = Mathf.FloorToInt(((position.y - (chunkHeight / 2f)) / chunkHeight) + 1);
+    //     return new Vector2Int(chunkX, chunkY);
+    // }
+    // public static Vector2Int GetChunkFromPosition(Vector3 position)
+    // {
+    //     return GetChunkFromPosition(new Vector2(position.x, position.y));
+    // }
 }
