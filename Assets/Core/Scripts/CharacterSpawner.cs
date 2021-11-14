687ac4b9e1730c2fc826fbb0a8cdbefd35342ef0
Oxters <oxters168@gmail.com>
2021-11-14 00:16:03 -0500

    Fixed an issue where character would sometimes automatically enter the vehicle directly after exiting a building. Fixed an issue with the door sometimes showing closed when actually open

--- Assets/Core/Scripts/CharacterSpawner.cs
+++ Assets/Core/Scripts/CharacterSpawner.cs
@@ -28,6 +28,7 @@ public class CharacterSpawner : MonoBehaviour
     public float exitVelocityUpwards = 0;
 
     private bool usedDoor;
+    private bool usedVehicle;
     
     void Start()
     {
@@ -82,14 +83,14 @@ public class CharacterSpawner : MonoBehaviour
     {
         if (player.GetButton("ButtonY"))
         {
-            if (!usedDoor)
+            if (!usedDoor && !usedVehicle)
             {
                 var doorDetector = spawnedCharacter.GetComponentInChildren<DoorDetector>();
                 if (doorDetector != null && doorDetector.door != null && doorDetector.door.GetComponent<Door>().isOpen)
                 {
+                    usedDoor = true;
                     terrain.isIndoors = !terrain.isIndoors;
                 }
-                usedDoor = true;
             }
         }
         else
@@ -97,76 +98,84 @@ public class CharacterSpawner : MonoBehaviour
     }
     private void EnterExitVehicle()
     {
-        if (player.GetButtonUp("ButtonY"))
+        if (player.GetButton("ButtonY"))
         {
-            bool inVehicle = controlledObject != spawnedCharacter.gameObject;
-            GameObject nearbyVehicle = null;
-            if (!inVehicle)
+            if (!usedVehicle && !usedDoor)
             {
-                var vehicleDetector = spawnedCharacter.GetComponentInChildren<VehicleDetector>();
-                if (vehicleDetector != null && vehicleDetector.vehicle != null)
-                    nearbyVehicle = vehicleDetector.vehicle.gameObject;
-            }
-
-            if (!inVehicle && nearbyVehicle != null) //If not in vehicle and there is a vehicle nearby then enter
-            {
-                spawnedCharacter.gameObject.SetActive(false);
-                controlledObject = nearbyVehicle;
-                spawnedCamera.target = nearbyVehicle.transform;
-            }
-            else if (inVehicle) //If in vehicle then exit
-            {
-                var vehicleBounds = controlledObject.transform.GetTotalBounds(Space.World);
-                var characterBounds = spawnedCharacter.transform.GetTotalBounds(Space.World, false, true);
-
-                //Get all possible exit positions
-                var sprite7Up = controlledObject.GetComponentInChildren<SpriteRenderer>();
-                bool onLeft = false;
-                if (sprite7Up != null)
-                    onLeft = sprite7Up.flipX;
-                Vector2 behindVehiclePosition = vehicleBounds.center + (onLeft ? 1 : -1) * Vector3.right * vehicleBounds.extents.x;
-                Vector2 frontVehiclePosition = vehicleBounds.center + (onLeft ? -1 : 1) * Vector3.right * vehicleBounds.extents.x;
-                Vector2 aboveVehiclePosition = vehicleBounds.center + Vector3.up * vehicleBounds.extents.y;
-                Vector2 belowVehiclePosition = vehicleBounds.center - Vector3.up * vehicleBounds.extents.y;
-
-                //Make sure exit position is not blocked
-                RaycastHit2D aboveHit = Physics2D.BoxCast(aboveVehiclePosition + Vector2.up * characterBounds.extents.y, characterBounds.extents, 0, Vector2.zero, 0, surroundingsMask);
-                RaycastHit2D bottomHit = Physics2D.BoxCast(belowVehiclePosition - Vector2.up * characterBounds.extents.y, characterBounds.extents, 0, Vector2.zero, 0, surroundingsMask);
-                RaycastHit2D behindHit = Physics2D.BoxCast(behindVehiclePosition + (onLeft ? 1 : -1) * Vector2.right * characterBounds.extents.x, characterBounds.extents, 0, Vector2.zero, 0, surroundingsMask);
-                RaycastHit2D frontHit = Physics2D.BoxCast(frontVehiclePosition + (onLeft ? -1 : 1) * Vector2.right * characterBounds.extents.x, characterBounds.extents, 0, Vector2.zero, 0, surroundingsMask);
-                // Debug.DrawRay(aboveVehiclePosition + Vector2.up * characterBounds.extents.y, Vector3.up, Color.green, 10);
-                // Debug.DrawRay(belowVehiclePosition - Vector2.up * characterBounds.extents.y, Vector3.up, Color.green, 10);
-                // Debug.DrawRay(behindVehiclePosition + (onLeft ? 1 : -1) * Vector2.right * characterBounds.extents.x, Vector3.up, Color.green, 10);
-                // Debug.DrawRay(frontVehiclePosition + (onLeft ? -1 : 1) * Vector2.right * characterBounds.extents.x, Vector3.up, Color.green, 10);
-
-                // Debug.Log((aboveHit == true) + ", " + (behindHit == true) + ", " + (frontHit == true) + ", " + (bottomHit == true));
-                Vector2 exitPosition = Vector2.zero;
-                if (aboveHit == false)
-                    exitPosition = aboveVehiclePosition;
-                else if (behindHit == false)
-                    exitPosition = behindVehiclePosition + (onLeft ? 1 : -1) * Vector2.right * characterBounds.extents.x;
-                else if (frontHit == false)
-                    exitPosition = frontVehiclePosition + (onLeft ? -1 : 1) * Vector2.right * characterBounds.extents.x;
-                else if (bottomHit == false)
-                    exitPosition = belowVehiclePosition - Vector2.up * characterBounds.size.y;
-
-                if (aboveHit == false || bottomHit == false || behindHit == false || frontHit == false) //If no obstacles then exit
+                // usedVehicle = true;
+                bool inVehicle = controlledObject != spawnedCharacter.gameObject;
+                GameObject nearbyVehicle = null;
+                if (!inVehicle) //If not currently in vehicle then check what vehicle is nearby
                 {
-                    var vehiclePhysics = controlledObject.GetComponentInChildren<Rigidbody2D>();
-                    var characterPhysics = spawnedCharacter.GetComponentInChildren<Rigidbody2D>();
-
-                    // spawnedCharacter.transform.position = aboveVehiclePosition;
-                    spawnedCharacter.transform.position = exitPosition;
-                    controlledObject = spawnedCharacter.gameObject;
-                    spawnedCharacter.gameObject.SetActive(true);
-                    spawnedCamera.target = spawnedCharacter.transform;
-
-                    if (characterPhysics != null && vehiclePhysics != null)
-                        characterPhysics.velocity = vehiclePhysics.velocity + Vector2.up * exitVelocityUpwards;
+                    var vehicleDetector = spawnedCharacter.GetComponentInChildren<VehicleDetector>();
+                    if (vehicleDetector != null && vehicleDetector.vehicle != null)
+                        nearbyVehicle = vehicleDetector.vehicle.gameObject;
                 }
-                else
-                    Debug.Log("Exit blocked");
+
+                if (!inVehicle && nearbyVehicle != null) //If not in vehicle and there is a vehicle nearby then enter
+                {
+                    usedVehicle = true;
+                    spawnedCharacter.gameObject.SetActive(false);
+                    controlledObject = nearbyVehicle;
+                    spawnedCamera.target = nearbyVehicle.transform;
+                }
+                else if (inVehicle) //If in vehicle then exit
+                {
+                    usedVehicle = true;
+                    var vehicleBounds = controlledObject.transform.GetTotalBounds(Space.World);
+                    var characterBounds = spawnedCharacter.transform.GetTotalBounds(Space.World, false, true);
+
+                    //Get all possible exit positions
+                    var sprite7Up = controlledObject.GetComponentInChildren<SpriteRenderer>();
+                    bool onLeft = false;
+                    if (sprite7Up != null)
+                        onLeft = sprite7Up.flipX;
+                    Vector2 behindVehiclePosition = vehicleBounds.center + (onLeft ? 1 : -1) * Vector3.right * vehicleBounds.extents.x;
+                    Vector2 frontVehiclePosition = vehicleBounds.center + (onLeft ? -1 : 1) * Vector3.right * vehicleBounds.extents.x;
+                    Vector2 aboveVehiclePosition = vehicleBounds.center + Vector3.up * vehicleBounds.extents.y;
+                    Vector2 belowVehiclePosition = vehicleBounds.center - Vector3.up * vehicleBounds.extents.y;
+
+                    //Make sure exit position is not blocked
+                    RaycastHit2D aboveHit = Physics2D.BoxCast(aboveVehiclePosition + Vector2.up * characterBounds.extents.y, characterBounds.extents, 0, Vector2.zero, 0, surroundingsMask);
+                    RaycastHit2D bottomHit = Physics2D.BoxCast(belowVehiclePosition - Vector2.up * characterBounds.extents.y, characterBounds.extents, 0, Vector2.zero, 0, surroundingsMask);
+                    RaycastHit2D behindHit = Physics2D.BoxCast(behindVehiclePosition + (onLeft ? 1 : -1) * Vector2.right * characterBounds.extents.x, characterBounds.extents, 0, Vector2.zero, 0, surroundingsMask);
+                    RaycastHit2D frontHit = Physics2D.BoxCast(frontVehiclePosition + (onLeft ? -1 : 1) * Vector2.right * characterBounds.extents.x, characterBounds.extents, 0, Vector2.zero, 0, surroundingsMask);
+                    // Debug.DrawRay(aboveVehiclePosition + Vector2.up * characterBounds.extents.y, Vector3.up, Color.green, 10);
+                    // Debug.DrawRay(belowVehiclePosition - Vector2.up * characterBounds.extents.y, Vector3.up, Color.green, 10);
+                    // Debug.DrawRay(behindVehiclePosition + (onLeft ? 1 : -1) * Vector2.right * characterBounds.extents.x, Vector3.up, Color.green, 10);
+                    // Debug.DrawRay(frontVehiclePosition + (onLeft ? -1 : 1) * Vector2.right * characterBounds.extents.x, Vector3.up, Color.green, 10);
+
+                    // Debug.Log((aboveHit == true) + ", " + (behindHit == true) + ", " + (frontHit == true) + ", " + (bottomHit == true));
+                    Vector2 exitPosition = Vector2.zero;
+                    if (aboveHit == false)
+                        exitPosition = aboveVehiclePosition;
+                    else if (behindHit == false)
+                        exitPosition = behindVehiclePosition + (onLeft ? 1 : -1) * Vector2.right * characterBounds.extents.x;
+                    else if (frontHit == false)
+                        exitPosition = frontVehiclePosition + (onLeft ? -1 : 1) * Vector2.right * characterBounds.extents.x;
+                    else if (bottomHit == false)
+                        exitPosition = belowVehiclePosition - Vector2.up * characterBounds.size.y;
+
+                    if (aboveHit == false || bottomHit == false || behindHit == false || frontHit == false) //If no obstacles then exit
+                    {
+                        var vehiclePhysics = controlledObject.GetComponentInChildren<Rigidbody2D>();
+                        var characterPhysics = spawnedCharacter.GetComponentInChildren<Rigidbody2D>();
+
+                        // spawnedCharacter.transform.position = aboveVehiclePosition;
+                        spawnedCharacter.transform.position = exitPosition;
+                        controlledObject = spawnedCharacter.gameObject;
+                        spawnedCharacter.gameObject.SetActive(true);
+                        spawnedCamera.target = spawnedCharacter.transform;
+
+                        if (characterPhysics != null && vehiclePhysics != null)
+                            characterPhysics.velocity = vehiclePhysics.velocity + Vector2.up * exitVelocityUpwards;
+                    }
+                    else
+                        Debug.Log("Exit blocked");
+                }
             }
         }
+        else
+            usedVehicle = false;
     }
 }
