3b907f68853e3a0a29030e919081e42700d3d512
Oxters <oxters168@gmail.com>
2021-12-02 20:24:36 -0500

    Removed refresh time from splitscreen script. Updated UnityHelpers.

--- Assets/Core/Scripts/Splitscreen.cs
+++ Assets/Core/Scripts/Splitscreen.cs
@@ -3,33 +3,24 @@ using System.Linq;
 
 public class Splitscreen : MonoBehaviour
 {
-    public float refreshTime = 3;
-    private float lastRefresh = float.MinValue;
-
     int prevCharacterCount;
 
     float prevScreenWidth, prevScreenHeight;
 
     void Update()
     {
-        if (Time.time - lastRefresh >= refreshTime)
+        var spawnedCharacters = CharacterRegistry.GetCurrentCharacters();
+        int currentCharacterCount = spawnedCharacters.Length;
+
+        if (prevCharacterCount != currentCharacterCount || Screen.width != prevScreenWidth || Screen.height != prevScreenHeight)//|| !Mathf.Approximately(Screen.width, prevScreenWidth) || !Mathf.Approximately(Screen.height, prevScreenHeight))
         {
-            var spawnedCharacters = CharacterRegistry.GetCurrentCharacters();
-            int currentCharacterCount = spawnedCharacters.Length;
+            var screenRects = BSP.Partition(new Rect(0, 0, Screen.width, Screen.height), (uint)currentCharacterCount).EnumerateRects().ToArray();
+            for (int cameraIndex = 0; cameraIndex < currentCharacterCount; cameraIndex++)
+                spawnedCharacters[cameraIndex].spawnedCamera.GetComponent<Camera>().rect = new Rect(screenRects[cameraIndex].x / Screen.width, screenRects[cameraIndex].y / Screen.height, screenRects[cameraIndex].width / Screen.width, screenRects[cameraIndex].height / Screen.height);
 
-            if (prevCharacterCount != currentCharacterCount || !Mathf.Approximately(Screen.width, prevScreenWidth) || !Mathf.Approximately(Screen.height, prevScreenHeight))
-            {
-                var screenRects = BSP.Partition(new Rect(0, 0, Screen.width, Screen.height), (uint)currentCharacterCount).EnumerateRects().ToArray();
-                for (int cameraIndex = 0; cameraIndex < currentCharacterCount; cameraIndex++)
-                    spawnedCharacters[cameraIndex].spawnedCamera.GetComponent<Camera>().rect = new Rect(screenRects[cameraIndex].x / Screen.width, screenRects[cameraIndex].y / Screen.height, screenRects[cameraIndex].width / Screen.width, screenRects[cameraIndex].height / Screen.height);
-
-                prevCharacterCount = currentCharacterCount;
-                prevScreenWidth = Screen.width;
-                prevScreenHeight = Screen.height;
-            }
-
-            lastRefresh = Time.time;
+            prevCharacterCount = currentCharacterCount;
+            prevScreenWidth = Screen.width;
+            prevScreenHeight = Screen.height;
         }
-
     }
 }
